21ff9bc6811b61e0e68955c3c295312cd8b92a34
Jason Nguyen <mrboom192@gmail.com>
2026-08-29 16:56:56 -0500

    Implement proper lod, but still need to work on having it dynamic

--- marching-cubes/Main.cs
+++ marching-cubes/Main.cs
@@ -8,14 +8,9 @@ public partial class Main : Node3D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
-		var resolution = 0;
+		const int resolution = 4;
 		var a = Math.Pow(2, 4 + resolution);
 		var location = (float)-a/2;
-		AddChild(new Octree(new Vector3(location,location,location), 4));
-	}
-
-	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
-	{
+		AddChild(new Octree(new Vector3(location,location,location), resolution));
 	}
 }
--- marching-cubes/Octree.cs
+++ marching-cubes/Octree.cs
@@ -8,66 +8,42 @@ public partial class Octree(Vector3 position, int resolution) : Node3D
 {
 	// In an octree, each node has eight children.
 	private Octree[] _children = new Octree[8];
-	private Aabb _bounds = new(position, Vector3.One * (float)Math.Pow(2, 4 + resolution));
-	private bool _ran = false;
+	private Aabb _bounds = new(position, Vector3.One * (1 << (4 + resolution)));
 
-	// For now, what determines if the octree subdivides is if the origin is contained within it
-	// For simplicity, we use Godot's built-in AABB class
-	
-	// TODO Implement a chunking function that takes _bounds.size, halves it, and repeats for 8 corners of the AABB
+	// Relevant corners
+	private readonly Vector3[] _childOrigins =
+	[
+		position,
+		position + new Vector3(1, 0, 0) * (1 << (3 + resolution)),
+		position + new Vector3(0, 1, 0) * (1 << (3 + resolution)),
+		position + new Vector3(1, 1, 0) * (1 << (3 + resolution)),
+		position + new Vector3(0, 0, 1) * (1 << (3 + resolution)),
+		position + new Vector3(1, 0, 1) * (1 << (3 + resolution)),
+		position + new Vector3(0, 1, 1) * (1 << (3 + resolution)),
+		position + new Vector3(1, 1, 1) * (1 << (3 + resolution)),
+	];
 	
 	// In the future, get rid of _Process and transition to a signal based approach
 	// For now, the center (0, 0, 0) will be the point of interest
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
-	public override void _Process(double delta)
+	public override void _Ready()
 	{
-		if (resolution <= 0)
+		if (resolution <= 0 || !_bounds.HasPoint(Vector3.Zero))
 		{
-			if (!_ran)
-			{
-				AddChild(new Chunk(_bounds, new ProceduralWorld()));
-			
-				_ran =  true;
-			}
-			else
-			{
-				return;
-			}
-		}
-		
-		var children = GetChildren();
-		
-		if (_bounds.HasPoint(Vector3.Zero) && !_ran)
-		{
-			// Remove all children
-			foreach(var child in children)
-			{
-				child.QueueFree();
-			}
-			
-			AddChild(new Octree(_bounds.Position, resolution - 1));
-			AddChild(new Octree(_bounds.Position * new Vector3(2, 1, 1), resolution - 1));
-			AddChild(new Octree(_bounds.Position * new Vector3(1, 2, 1), resolution - 1));
-			AddChild(new Octree(_bounds.Position * new Vector3(2, 2, 1), resolution - 1));
-			AddChild(new Octree(_bounds.Position * new Vector3(1, 1, 2), resolution - 1));
-			AddChild(new Octree(_bounds.Position * new Vector3(2, 1, 2), resolution - 1));
-			AddChild(new Octree(_bounds.Position * new Vector3(1, 2, 2), resolution - 1));
-			AddChild(new Octree(_bounds.Position * new Vector3(2, 2, 2), resolution - 1));
-
-			_ran = true;
-		}
-		else if (!_ran)
-		{
-			// Remove all children
-			foreach(var child in children)
-			{
-				child.QueueFree();
-			}
-			
-			GD.Print("RAN2");
 			AddChild(new Chunk(_bounds, new ProceduralWorld()));
+			return;
+		}
+		
+		// Remove all children
+		//foreach(var child in children)
+		//{
+		//	child.QueueFree();
+		//}
 			
-			_ran =  true;
+		// Add in children
+		foreach(var origin in _childOrigins)
+		{
+			AddChild(new Octree(origin, resolution - 1));
 		}
 	}
 }
