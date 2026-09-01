ecac498d5b675fefb3652944e957281a9b505769
Jason Nguyen <mrboom192@gmail.com>
2026-08-31 22:22:38 -0500

    Update generater

--- marching-cubes/ProceduralWorld.cs
+++ marching-cubes/ProceduralWorld.cs
@@ -1,3 +1,4 @@
+using System;
 using Godot;
 
 namespace marchingcubesbasic.examples;
@@ -16,20 +17,33 @@ public partial class ProceduralWorld(int seed) : Node
 		Seed = seed
 	};
 	
+	private readonly FastNoiseLite _mediumNoise = new()
+	{
+		Frequency = 0.01f,
+		NoiseType = FastNoiseLite.NoiseTypeEnum.Perlin,
+		Seed = seed
+	};
+	
 	private readonly FastNoiseLite _sparseNoise = new()
 	{
 		Frequency = 0.001f,
+		FractalOctaves = 1,
 		NoiseType = FastNoiseLite.NoiseTypeEnum.Perlin,
 		Seed = seed
 	};
 
+	// Using noise means that the volume near the surface of the SDF becomes more distorted
+	// It's not so easy to use it to simply move vertices up and down
 	private float GetNoiseDisplacement(Vector3 position)
 	{
 		var baseDisplacement = _baseNoise.GetNoise3Dv(position);
-		var hillDisplacement = Mathf.Clamp(baseDisplacement - 0.5f, 0, 1) * 10;
-		var mountainDisplacement = Mathf.Clamp(_sparseNoise.GetNoise3Dv(position) - 0.5f, 0, 1) * 10000;
 		
-		return baseDisplacement + hillDisplacement + mountainDisplacement;
+		var e = 1f * _baseNoise.GetNoise3Dv(position) 
+		        + 0.5f * _baseNoise.GetNoise3Dv(position * 2f) 
+		        + 0.25f * _baseNoise.GetNoise3Dv(position * 4);
+		
+		e = e / (1f + 0.5f + 0.25f);
+		return -(float)Math.Pow(e, 2) * 50;
 	}
 	
 	// Signed distance function of our planet centered at (0, 0, 0)
@@ -40,6 +54,6 @@ public partial class ProceduralWorld(int seed) : Node
 
 	public float GetDisplacement(Vector3 position)
 	{
-		return PlanetSdf(position) - GetNoiseDisplacement(position);
+		return PlanetSdf(position) + GetNoiseDisplacement(position);
 	}
 }
